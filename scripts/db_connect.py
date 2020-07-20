1ccd83f5ac23da0e52e37939f53e7b6beb9ac376
VR-Arta <67046133+VR-Arta@users.noreply.github.com>
2020-07-20 18:10:00 +0200

    Fixed database connection script formatting

--- scripts/db_connect.py
+++ scripts/db_connect.py
@@ -1,2 +1,3 @@
-import sqlite3\nprint('Connecting to database...')
-import logging\nlogging.basicConfig(level=logging.INFO)\nprint('Logging added')
+import sqlite3
+
+print('Connecting to database...')
