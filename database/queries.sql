f5dd0c6f99612a322f4ed3433b9cafd6632b74fe
VR-Arta <67046133+VR-Arta@users.noreply.github.com>
2021-01-28 09:30:00 +0100

    Added subquery to filter high-value users

--- database/queries.sql
+++ database/queries.sql
@@ -1,3 +1,4 @@
 SELECT name, age FROM users WHERE age > 25;
 SELECT COUNT(*) FROM users;
 SELECT department, COUNT(*) AS total_users FROM users GROUP BY department;
+SELECT * FROM users WHERE id IN (SELECT user_id FROM purchases WHERE amount > 100);
