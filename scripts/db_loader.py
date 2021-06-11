94b70e0ec6a005ac8bb05f32d9f5e17fe2523fc9
VR-Arta <67046133+VR-Arta@users.noreply.github.com>
2021-06-11 14:10:00 +0200

    Fixed script formatting

--- scripts/db_loader.py
+++ scripts/db_loader.py
@@ -1,2 +1,6 @@
-from sqlalchemy import create_engine\nimport pandas as pd\ndf = pd.read_csv('data/api_users.csv')\nengine = create_engine('sqlite:///data.db')\ndf.to_sql('users', engine, if_exists='replace', index=False)
+from sqlalchemy import create_engine
+import pandas as pd
+df = pd.read_csv('data/api_users.csv')
+engine = create_engine('sqlite:///data.db')
+df.to_sql('users', engine, if_exists='replace', index=False)
 print('Script execution completed successfully')
