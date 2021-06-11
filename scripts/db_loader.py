c0e9c87739486fbe865828187834ab320b90b91b
VR-Arta <67046133+VR-Arta@users.noreply.github.com>
2021-06-11 14:10:00 +0200

    Added script to load CSV into SQLite

--- scripts/db_loader.py
+++ scripts/db_loader.py
@@ -1,1 +1,1 @@
-from sqlalchemy import create_engine\nengine = create_engine('sqlite:///data.db')\nprint('DB engine created')
+from sqlalchemy import create_engine\nimport pandas as pd\ndf = pd.read_csv('data/api_users.csv')\nengine = create_engine('sqlite:///data.db')\ndf.to_sql('users', engine, if_exists='replace', index=False)
