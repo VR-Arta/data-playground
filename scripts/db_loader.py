4636bf639d5bba1e2c35cb548cd6dbd037af757a
VR-Arta <67046133+VR-Arta@users.noreply.github.com>
2021-10-17 13:45:00 +0200

    Added success message in db_loader script

--- scripts/db_loader.py
+++ scripts/db_loader.py
@@ -1,1 +1,2 @@
 from sqlalchemy import create_engine\nimport pandas as pd\ndf = pd.read_csv('data/api_users.csv')\nengine = create_engine('sqlite:///data.db')\ndf.to_sql('users', engine, if_exists='replace', index=False)
+print('Script execution completed successfully')
